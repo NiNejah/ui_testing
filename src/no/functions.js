e8c5055b045526b37e5c7e39f80cbb69a9bbb7cb
ahmatali <ali-abdoulaye.ahmat@etu.u-bordeaux.fr>
2023-03-21 14:52:05 +0100

    add dossier for command

---
+++ src/no/functions.js
@@ -0,0 +1,15 @@
+function maximum(a,b){
+    return a>b ? a : b;
+}
+
+
+function minimum(a,b){
+    return a<b ? a : b;
+}
+
+
+function radu(a,r){
+    return a  + r ;
+}
+
+module.exports = {radu,minimum,maximum} ;
