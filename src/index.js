0d6b3c946a651c6c55b0ecbf218c1ae025b2d847
NiNejah <almawlanejeh@gmail.com>
2023-04-04 19:35:01 +0200

    fix : index.js

--- src/index.js
+++ src/index.js
@@ -13,28 +13,28 @@ const driver = require('./config/driver');
 
 const upload = multer({ dest: 'uploads/' });
 
-let cmds = [] ; 
-let allRes = [];
-let filePath =''
 
+let clientConnected = false ; 
 
 app.post('/runTest', upload.single('myFile'), async (req, res) => {
+    let cmds = [] ; 
+    let allRes = [];
     // console.log(res); // contains information about the uploaded file
     const file = req.file;
-    filePath = file.path ; 
-    const fileContent = fs.readFileSync(filePath, 'utf8');
+    let filePath = file.path ; 
     // res.send("file loaded !");
-    await runTest(filePath);
-    res.render('runTests',{cmds, allRes});
-    // // // Pipe the read stream to the response stream to send the file to the client
-    // readStream.pipe(res);
-
-    // // // Delete the uploaded file from the server after it has been sent to the client
-    // fs.unlink(filePath, (err) => {
-    //     if (err) {
-    //     console.error(err);
-    //     }
-    // });
+    try {
+        await runTest(filePath,cmds,allRes);
+        res.render('runTests',{cmds, allRes});
+    }catch (err){
+        res.send("invalide test file or command !");
+    }finally {
+        fs.unlink(file.path, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
 });
 
 // set EJS as templating engine
@@ -53,43 +53,33 @@ app.get('/', (req, res) => {
 });
 
 
-// app.get('/runTests', async (req, res) => {
-//     await runTest(filePath);
-//     res.render('runTests',{cmds, allRes});
-// });
 
-// app.post('/upload', upload.single('myFile'), (req, res) => {
-//     console.log(req.file); // contains information about the uploaded file
-//     res.send('File uploaded successfully!');
-//   });
-
-const runTest = async (filePath) => {
-    try {
+const runTest = async (filePath,cmds,allRes) => {
+    if (!clientConnected){
         await client.connect();
-    }catch(er){
-
-    }finally {
-        const fileContent = fs.readFileSync(filePath, 'utf8');
-        let commandBlocks = tools.parseText(fileContent);
-        for (const cmdBlock of commandBlocks){
+        clientConnected = true ; 
+    }
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    let commandBlocks = tools.parseText(fileContent);
+    for (const cmdBlock of commandBlocks){
+        try {
             let cmdObj = Command.create(cmdBlock) ; 
             if(cmdObj !== null){
                 await cmds.push(cmdObj); 
             }
+        }catch (err){
+            throw new Error(err);
         }
-        // this loop will execute all test command 
-        // that's well display the comand with it's toString and : PASS if the test pass or : NOT PASS     
-        for (let e of cmds){
-            let res = await e.execute(); 
-            allRes.push(res);
-            console.log(res.testDescription);
-            // maketestDescription(e.toString,res.isPass);
-            if (!res.isPass){
-                break ;
-            }
+    }   
+    for (let e of cmds){
+        let res = await e.execute(); 
+        allRes.push(res);
+        // console.log(res.testDescription);
+        if (!res.isPass){
+            break ;
         }
-        await client.end();
-        await driver.close();
-        console.log ("tests end ...");
     }
+    // await driver.close();
+    console.log ("tests end ...");
+    
 }
